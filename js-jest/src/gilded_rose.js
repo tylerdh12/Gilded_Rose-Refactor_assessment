c96d209ab71df9077d290c0b0538beac86e63831
Tyler Harper <tylerdh12@gmail.com>
2022-04-24 19:17:40 -0700

    Refactor the original codebase

--- js-jest/src/gilded_rose.js
+++ js-jest/src/gilded_rose.js
@@ -1,67 +1,61 @@
 class Item {
-  constructor(name, sellIn, quality){
-    this.name = name;
-    this.sellIn = sellIn;
-    this.quality = quality;
-  }
+	constructor(name, sellIn, quality) {
+		this.name = name
+		this.sellIn = sellIn
+		this.quality = quality
+	}
 }
 
 class Shop {
-  constructor(items=[]){
-    this.items = items;
-  }
-  updateQuality() {
-    for (let i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-            this.items[i].quality = this.items[i].quality - 1;
-          }
-        }
-      } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1;
-          if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-          }
-        }
-      }
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
-      }
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie') {
-          if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].quality = this.items[i].quality - 1;
-              }
-            }
-          } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality;
-          }
-        } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1;
-          }
-        }
-      }
-    }
+	constructor(items = []) {
+		this.items = items
+	}
+	updateQuality() {
+		let updatedItems = []
 
-    return this.items;
-  }
+		this.items.reduce((_, currentValue) => {
+			switch (currentValue.name) {
+				case 'Aged Brie':
+					currentValue.sellIn <= 0 ? (currentValue.quality += 2) : (currentValue.quality += 1)
+					currentValue.quality >= 50 ? (currentValue.quality = 50) : currentValue.quality
+					currentValue.quality <= 0 ? (currentValue.quality = 0) : currentValue.quality
+					currentValue.sellIn--
+					break
+
+				case 'Backstage passes to a TAFKAL80ETC concert':
+					currentValue.sellIn >= 11 ? (currentValue.quality += 1) : (currentValue.quality += 2)
+					currentValue.sellIn >= 6 ? currentValue.quality : (currentValue.quality += 1)
+					currentValue.quality <= 0 ? (currentValue.quality = 0) : currentValue.quality
+					currentValue.quality >= 50 ? (currentValue.quality = 50) : currentValue.quality
+					currentValue.sellIn <= 0 ? (currentValue.quality = 0) : currentValue.quality
+					currentValue.sellIn--
+					break
+
+				case 'Sulfuras, Hand of Ragnaros':
+					break
+
+				case 'Conjured':
+					currentValue.sellIn <= 0 ? (currentValue.quality -= 4) : (currentValue.quality -= 2)
+					currentValue.quality >= 50 ? (currentValue.quality = 50) : currentValue.quality
+					currentValue.quality <= 0 ? (currentValue.quality = 0) : currentValue.quality
+					currentValue.sellIn--
+					break
+
+				default:
+					currentValue.sellIn <= 0 ? (currentValue.quality -= 2) : (currentValue.quality -= 1)
+					currentValue.quality >= 50 ? (currentValue.quality = 50) : currentValue.quality
+					currentValue.quality <= 0 ? (currentValue.quality = 0) : currentValue.quality
+					currentValue.sellIn--
+			}
+
+			updatedItems.push(currentValue)
+		}, [])
+
+		return updatedItems
+	}
 }
 
 module.exports = {
-  Item,
-  Shop
+	Item,
+	Shop
 }
